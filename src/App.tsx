20f0c494fae4e4597bdd2d3a59f74da0c8f44df6
enamulhaqe7 <enamulhaquemb01@gmail.com>
2021-09-01 14:45:59 -0700

    updating

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,8 @@ function App() {
     <div>
       <h1>Hello Typescript</h1>
       <h1>Hello</h1>
-
-      <p>Lorem ipsum dolor sit amet.</p>
-      <h2>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quos
-        deleniti nisi maxime exercitationem molestias quisquam vitae
-        perspiciatis esse distinctio expedita quibusdam asperiores aliquam iure,
-        blanditiis rem doloribus fuga nemo.
-      </h2>
-      <span>kire</span>
+      <p>Lorem ipsum dolor sit .</p>
+      <b>Hello</b>
     </div>
   );
 }
