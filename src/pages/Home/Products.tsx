3b583f9f5cd8e5ea83932072b4a716a637aef510
enamulhaqe7 <enamulhaquemb01@gmail.com>
2021-09-22 22:00:41 +0600

    updating

--- src/pages/Home/Products.tsx
+++ src/pages/Home/Products.tsx
@@ -7,7 +7,7 @@ const Products = () => {
       <Card>
         <Card.Img
           variant="top"
-          src="https://images.unsplash.com/photo-1627062598433-016841c1f1e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
+          src="https://images.unsplash.com/photo-1628191081698-44f573462a03?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
         />
         <Card.Body>
           <Card.Title>
@@ -15,7 +15,6 @@ const Products = () => {
             Kovix SS Acid Proof Bike Disk Lock for Motorbike - Lime -{' '}
           </Card.Title>
           <p>$ 200</p>
-          <p>$ 150</p>
         </Card.Body>
       </Card>
     </Col>
